764be1b3d21b94d34935d106ad2efce309b867bf
Tarhe <owehtarhe@gmail.com>
2022-12-22 10:15:43 +0000

    added a divider

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -2,19 +2,22 @@
 const Journal = function Journal(props)
 {
     return (
-        <section>
-            <img src={props.imageUrl} alt={props.title} className="travel-img"/>
-            <div className="container">
-                <span>
-                    <img src="images/location.png" alt="location-icon" className=""/>
-                    <span className='location'> {props.location}</span>
-                    <span> <a href={props.googleMapUrl} className='map'>View on Google Maps</a></span>
-                </span>
-                <h2 className='title'>{props.title}</h2>
-                <span className='date'>{props.startDate} - {props.endDate}</span>
-                <p className='description'>{props.description}</p>
-            </div>
-        </section>
+        <div className="App">
+            <section>
+                <img src={props.imageUrl} alt={props.title} className="travel-img"/>
+                <div className="container">
+                    <span>
+                        <img src="images/location.png" alt="location-icon" className=""/>
+                        <span className='location'> {props.location}</span>
+                        <span> <a href={props.googleMapUrl} className='map'>View on Google Maps</a></span>
+                    </span>
+                    <h2 className='title'>{props.title}</h2>
+                    <span className='date'>{props.startDate} - {props.endDate}</span>
+                    <p className='description'>{props.description}</p>
+                </div>
+            </section>
+            <hr className="divider"/>
+        </div>
     )
 }
 
