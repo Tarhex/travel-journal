72a6ba7d9ea524af1462c7ccef43f66c6736c925
Tarhe <owehtarhe@gmail.com>
2022-12-22 09:57:25 +0000

    travel journal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import Journal from './components/Journal'
+import Entry from './components/Entry'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const journalEntry = Entry.map(entry =>{
+        return <Journal
+					{...entry}
+				/>
+    })
+
+	return (
+		<div className="App">
+			<Header />
+			{journalEntry}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = function Header(){
+    return (
+            <nav className='nav-bar'>
+                <div className='nav-bar-content'>
+                    <img src="images/logo.png" alt="logo" className="nav-img"/>
+                    <h4 className='website-title'>my travel journal.</h4>
+                </div>
+            </nav>
+        )
+}
+
+export default Header
